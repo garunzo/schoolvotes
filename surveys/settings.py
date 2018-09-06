8a75d47a9b34f1a1bb6101228cbf3eacd63e132e
Luca C <luca.a.cotter@gmail.com>
2018-09-05 21:15:10 -0700

    adding https

--- surveys/settings.py
+++ surveys/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sslserver',
 ]
 
 SITE_ID=1
@@ -172,3 +173,7 @@ SOCIALACCOUNT_PROVIDERS = {
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
