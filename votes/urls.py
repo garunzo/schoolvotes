685c63d93d2f5142103bdcb5a42709d460696f64
Luca C <luca.a.cotter@gmail.com>
2019-05-04 17:45:25 -0700

    adding battleroyale to base and urls.py

--- votes/urls.py
+++ votes/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path("contact", views.contact, name="contact"),
     path("privacy", views.privacy, name="privacy"),
     path("dump_votes", views.dump_votes, name="dump_votes"),
+    path("battleroyale", views.battleroyale, name="battleroyale"),
 
     path("community/<int:community_id>", views.select_community, name="community"),
     path("survey/<int:survey_id>", views.select_survey, name="survey"),
